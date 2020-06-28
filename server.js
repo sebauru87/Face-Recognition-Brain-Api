3fdde279557ab236ed7359c8b6dde7ae64fecc55
Sebastian <Ferreira>
2020-06-28 00:29:48 -0300

    Add postgres sql to heroku

--- server.js
+++ server.js
@@ -12,11 +12,9 @@ const express = require("express"),
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "",
-    database: "smart-brain",
-  },
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+  }
 });
 
 db.select("*")
